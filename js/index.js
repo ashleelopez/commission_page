174e48f480a2a557473b754c61faff1064131224
Ashlee Lopez Santiago <111544992+ashleelopez@users.noreply.github.com>
2023-05-06 21:50:27 +0000

    Sat, May 6, 2023, 2:50 PM -07:00

--- js/index.js
+++ js/index.js
@@ -1,23 +1,4 @@
-const loadText = document.querySelector('.loading-text')
-const bg = document.querySelector('.bg')
 
-
-let load = 0
-
-let int = setInterval(loading, 40)
-
-
-
-
-function loading(){
-    load++
-    loadText.innerText = `${load}%`
-    bg.style.filter = `blur(${30 - 0.3*load}px)`
-    loadText.style.opacity = `${1 - load/100}`
-    if (load == 100){
-        clearInterval(int)
-    }
-}
 
 const menubar = document.getElementById('menubar')
 const sidenav = document.getElementById('sidenav')
